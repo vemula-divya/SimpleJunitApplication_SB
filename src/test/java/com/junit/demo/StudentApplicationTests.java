a0394be4ce87f5395c1c6e7e7929dce588642314
Divya Vemula <116681999+vemula-divya@users.noreply.github.com>
2023-07-08 00:46:59 -0700

    Update StudentApplicationTests.java

--- src/test/java/com/junit/demo/StudentApplicationTests.java
+++ src/test/java/com/junit/demo/StudentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.junit.demo;
+/*package com.junit.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class StudentApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
